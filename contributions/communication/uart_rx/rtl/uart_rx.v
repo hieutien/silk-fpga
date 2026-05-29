a31b6856c316a02636a2866fd7826d61794af41f
HUỲNH QUANG HUY <huynhquanghuy2904@gmail.com>
2026-05-29 13:50:37 +0700

    Update uart_rx.v

--- contributions/communication/uart_rx/rtl/uart_rx.v
+++ contributions/communication/uart_rx/rtl/uart_rx.v
@@ -3,11 +3,11 @@ module uart_rx (
     input rst_n,
     output reg [7:0] o_data
 );
-    // Verilator se canh bao neu dung gan bang (=) trong block always posedge
+    
     always @(posedge clk) begin
         if (!rst_n)
             o_data <= 8'h00;
         else
-            o_data <= 8'hFF; // Sua luon thanh gan lenh (<=) cho dung logic non-blocking
+            o_data <= 8'hFF; 
     end
 endmodule
